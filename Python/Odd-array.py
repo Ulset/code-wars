fb46d12ffc3d1da5e54e8fad0d6f0e75b3f736f6
Sander Ulset <sanderulset1@gmail.com>
2020-12-03 18:54:14 +0100

    Odd array

---
+++ Python/Odd-array.py
@@ -0,0 +1,16 @@
+"""
+Given an array of integers, find the one that appears an odd number of times.
+
+There will always be only one integer that appears an odd number of times.
+"""
+
+def find_it(seq):
+    nums = {}
+    for num in seq:
+        try:
+            nums[num] = nums[num] + 1
+        except KeyError:
+            nums[num] = 1
+    for numKey in nums:
+        if(nums[numKey]%2 != 0):
+            return numKey
